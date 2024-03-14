2d8f83f8700d8f978161cea800a6b7a5433e636d
Ethan Freeman <92946781+ethanfree23@users.noreply.github.com>
2024-03-14 18:07:25 +0000

    logos and some copy

--- src/App.js
+++ src/App.js
@@ -14,6 +14,11 @@ import { CheckIcon } from '@heroicons/react/20/solid'
 import LogoImg from './img/logo.png'
 import AutomationImg from './img/automation.jpg'
 import OrderingDashboardImg from './img/orderingDashboard.png'
+import DelLogo from './img/logoDel.png'
+import MadeWestLogo from './img/logoMadeWest.png'
+import SchneiderLogo from './img/logoSchneider.png'
+import PalmerLogo from './img/logoPalmer.png'
+import CircleBreweryLogo from './img/logoCircleBrewery.png'
 
 const navigation = [
   { name: 'About', href: '#about-section' },
@@ -351,35 +356,35 @@ function App() {
           <div className="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-5">
             <img
               className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-              src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
+              src={DelLogo}
               alt="Transistor"
               width={158}
               height={48}
             />
             <img
               className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-              src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
+              src={SchneiderLogo}
               alt="Reform"
               width={158}
               height={48}
             />
             <img
               className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-              src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
+              src={PalmerLogo}
               alt="Tuple"
               width={158}
               height={48}
             />
             <img
               className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-              src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
+              src={MadeWestLogo}
               alt="SavvyCal"
               width={158}
               height={48}
             />
             <img
               className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-              src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
+              src={CircleBreweryLogo}
               alt="Statamic"
               width={158}
               height={48}
@@ -388,10 +393,10 @@ function App() {
           <div className="mt-16 flex justify-center">
             <p className="relative rounded-full px-4 py-1.5 text-sm leading-6 text-gray-600 ring-1 ring-inset ring-gray-900/10 hover:ring-gray-900/20">
               <span className="hidden md:inline">
-                Transistor saves up to $40,000 per year, per employee by working with us.
+                OEMs, distributors, and CPGs save money, spend less, and are happier.
               </span>
               <a href="#" className="font-semibold text-blue-500">
-                <span className="absolute inset-0" aria-hidden="true" /> Read our case study{' '}
+                <span className="absolute inset-0" aria-hidden="true" /> Learn more{' '}
                 <span aria-hidden="true">&rarr;</span>
               </a>
             </p>
