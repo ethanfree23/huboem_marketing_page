a649486256d1b3f2f1041f764731bb4a3bfa840a
Ethan Freeman <92946781+ethanfree23@users.noreply.github.com>
2024-03-24 20:27:22 +0000

    fixed blog

--- src/pages/Company/Blog.js
+++ src/pages/Company/Blog.js
@@ -1,19 +1,19 @@
 const posts = [
     {
         id: 1,
-        title: 'Boost your conversion rate',
+        title: 'Boost your aftermarket sales',
         href: '#',
         description:
-            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
-        date: 'Mar 16, 2020',
-        datetime: '2020-03-16',
-        category: { title: 'Marketing', href: '#' },
+            'Turns out, in 2024, consumers prefer to purchase goods and services through an online platform, as oppossed to calling or emailing to place an order. This article will provide some evidence, as well as some tips for how to change this.',
+        date: 'Mar 16, 2024',
+        datetime: '2024-03-16',
+        category: { title: 'Marketing', href: '/marketing/blog/Boost_your_aftermarket_sales' },
         author: {
-            name: 'Michael Foster',
-            role: 'Co-Founder / CTO',
+            name: 'Alex Johnston',
+            role: 'CEO',
             href: '#',
             imageUrl:
-                'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+                'https://media.licdn.com/dms/image/C5603AQHbuSqS2VxgMg/profile-displayphoto-shrink_400_400/0/1517793948556?e=1717027200&v=beta&t=OpyXNZLecwINFg7-uIrIEPBmKuEZd3tBhpKMTM0b6yc',
         },
     },
     // More posts...
@@ -26,7 +26,7 @@ function Blog() {
                 <div className="mx-auto max-w-2xl">
                     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
                     <p className="mt-2 text-lg leading-8 text-gray-600">
-                        Learn how to grow your business with our expert advice.
+                        Learn how to grow your aftermarket business with our expert advice.
                     </p>
                     <div className="mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16">
                         {posts.map((post) => (
