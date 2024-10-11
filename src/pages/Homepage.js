a596fa55d18f3a16d25f18eb033287c4998dffb0
Ethan Freeman <92946781+ethanfree23@users.noreply.github.com>
2024-10-11 00:37:38 +0000

    get started link

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -198,7 +198,7 @@ function Homepage() {
                                 </p>
                                 <div className="mt-10 flex items-center justify-center gap-x-6">
                                     <a
-                                        href="#signup-section"
+                                        href="https://calendly.com/ethan-huboem/30min"
                                         className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500"
                                     >
                                         Get started
